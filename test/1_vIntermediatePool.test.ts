86392be23199745ce6d28eab56843c16852959e5
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2022-12-26 19:33:36 +0300

    Add tests for phase 2

--- test/1_vIntermediatePool.test.ts
+++ test/1_vIntermediatePool.test.ts
@@ -181,5 +181,124 @@ describe('vIntermediatePool: Phase 1', function () {
 });
 
 describe('vIntermediatePool: Phase 2', function () {
-    beforeEach(async () => {});
+    let intermediatePoolFactory: vIntermediatePoolFactory;
+    let intermediatePool: vIntermediatePool;
+    let mockUniswapOracle: MockUniswapOracle;
+    let mockV3Aggregator0: MockV3Aggregator0;
+    let mockV3Aggregator1: MockV3Aggregator1;
+    let mockVPairFactory: MockVPairFactory;
+    let token0: Token0;
+    let token1: Token1;
+    let deployer: SignerWithAddress;
+    let accounts;
+
+    beforeEach(async () => {
+        accounts = await ethers.getSigners();
+        deployer = accounts[0];
+        await deployments.fixture(['all']);
+        intermediatePoolFactory = await ethers.getContract(
+            'intermediatePoolFactory'
+        );
+        mockVPairFactory = await ethers.getContract('MockVPairFactory');
+        mockUniswapOracle = await ethers.getContract('MockUniswapOracle');
+        mockV3Aggregator0 = await ethers.getContract('MockV3Aggregator0');
+        mockV3Aggregator1 = await ethers.getContract('MockV3Aggregator1');
+        token0 = await ethers.getContract('Token0');
+        token1 = await ethers.getContract('Token1');
+        await mockVPairFactory.createPair(token0.address, token1.address);
+        await intermediatePoolFactory.createPool(
+            token0.address,
+            token1.address,
+            mockUniswapOracle.address,
+            mockV3Aggregator0.address,
+            mockV3Aggregator1.address,
+            await time.latest()
+        );
+        const intermediatePoolAddress = await intermediatePoolFactory.getPool(
+            token0.address,
+            token1.address
+        );
+        const factory = await ethers.getContractFactory('vIntermediatePool');
+        intermediatePool = await factory.attach(intermediatePoolAddress);
+
+        const pair = (await ethers.getContractFactory('MockVPair')).attach(
+            await mockVPairFactory.getPair(token0.address, token1.address)
+        );
+
+        await intermediatePool.triggerDepositPhase();
+        await token0.approve(
+            intermediatePool.address,
+            ethers.utils.parseEther('1000')
+        );
+        await token1.approve(
+            intermediatePool.address,
+            ethers.utils.parseEther('1000')
+        );
+        // Deposit phase
+
+        let amount0 = ethers.utils.parseEther('10');
+        let amount1 = ethers.utils.parseEther('10');
+        for (const account of accounts) {
+            await token0.mint(account.address, ethers.utils.parseEther('1000'));
+            await token1.mint(account.address, ethers.utils.parseEther('1000'));
+            await token0
+                .connect(account)
+                .approve(
+                    intermediatePool.address,
+                    ethers.utils.parseEther('1000')
+                );
+            await token1
+                .connect(account)
+                .approve(
+                    intermediatePool.address,
+                    ethers.utils.parseEther('1000')
+                );
+            amount0 = amount0.add(ethers.utils.parseEther('10'));
+            amount1 = amount1.add(ethers.utils.parseEther('10'));
+            await intermediatePool
+                .connect(account)
+                .deposit(amount0, amount1, 4);
+        }
+        await intermediatePool.deposit(amount0, amount1, 8);
+        await intermediatePool
+            .connect(accounts[2])
+            .deposit(amount0, amount1, 8);
+        await intermediatePool
+            .connect(accounts[1])
+            .deposit(amount0, amount1, 2);
+        await time.setNextBlockTimestamp(
+            (await time.latest()) + 7 * 24 * 60 * 60
+        );
+        await intermediatePool.triggerTransferPhase();
+    });
+
+    it('Transfer all deposits in 1 transaction', async () => {
+        await intermediatePool.transferToRealPool(10000);
+        // all tokens were transferred
+        expect(await token0.balanceOf(intermediatePool.address)).to.equal(0);
+        expect(await token1.balanceOf(intermediatePool.address)).to.equal(0);
+        // phase transition happened
+        expect(await intermediatePool.currentPhase()).to.equal(3);
+        // lp tokens were received
+        expect(await intermediatePool.totalLpTokens()).to.equal(100);
+    });
+
+    it('Transfer all deposits in 3 transactions', async () => {
+        await intermediatePool.transferToRealPool(6);
+        await intermediatePool.transferToRealPool(10);
+        await intermediatePool.transferToRealPool(10);
+        // all tokens were transferred
+        expect(await token0.balanceOf(intermediatePool.address)).to.equal(0);
+        expect(await token1.balanceOf(intermediatePool.address)).to.equal(0);
+        // phase transition happened
+        expect(await intermediatePool.currentPhase()).to.equal(3);
+        // lp tokens were received
+        expect(await intermediatePool.totalLpTokens()).to.equal(300);
+    });
+
+    it('Must revert if transfers amount is zero', async () => {
+        await expect(intermediatePool.transferToRealPool(0)).to.revertedWith(
+            'Transfers number must be positive'
+        );
+    });
 });
