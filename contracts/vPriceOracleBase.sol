b1b47a5b3367cd527544697407c3a8a44e9bcbdb
Ilya Kapitonau <ilya.th.kapitonov@gmail.com>
2023-01-09 13:09:29 +0300

    Adjust uniswap oracle time period

--- contracts/vPriceOracleBase.sol
+++ contracts/vPriceOracleBase.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.0;
 
 abstract contract vPriceOracleBase {
     uint8 public constant PRICE_RATIO_SHIFT_SIZE = 32;
-    uint32 public constant UNISWAP_ORACLE_TIME_PERIOD = 60;
+    uint32 public constant UNISWAP_ORACLE_TIME_PERIOD = 3600;
 }
